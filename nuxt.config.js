5c34f91ad64f718ff6ffe82fd1a0a0c57a4ba2c9
Farid Azhari <faridazhari07@gmail.com>
2024-06-19 06:33:36 +0700

    fix get data server erorr

--- nuxt.config.js
+++ nuxt.config.js
@@ -140,12 +140,12 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: '/',
+    baseURL: 'http://localhost:8000',
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-    baseURL: 'http://localhost:8000'
+    baseURL: '/'
   },
 
   loading: {
