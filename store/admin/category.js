4120e4e4226e9acf776cc4da5fad8494fda00f67
Farid Azhari <faridazhari07@gmail.com>
2024-06-20 09:52:51 +0700

    add update category

--- store/admin/category.js
+++ store/admin/category.js
@@ -7,6 +7,9 @@ export const state = () => ({
   //page
   page: 1,
 
+   //category
+   category: {}
+
 })
 
 //mutations
@@ -25,6 +28,15 @@ export const mutations = {
       //set value state "page"
       state.page = payload
   },
+
+    //mutation "SET_CATEGORY_DATA"
+    SET_CATEGORY_DATA(state, payload) {
+
+      //set value state "category"
+      state.category = payload
+  },
+
+
 
 }
 
@@ -85,4 +97,55 @@ export const actions = {
       })
   },
 
+ //get detail category
+ getDetailCategory({ commit }, payload) {
+
+  //set promise
+  return new Promise((resolve, reject) => {
+
+      //get to Rest API "/api/admin/categories/:id" with method "GET"
+      this.$axios.get(`/api/admin/categories/${payload}`)
+
+      //success
+      .then(response => {
+
+          //commit to mutation "SET_CATEGORY_DATA"
+          commit('SET_CATEGORY_DATA', response.data.data)
+
+          //resolve promise
+          resolve()
+
+      })
+
+  })
+
+},
+
+  //update category
+  updateCategory({ dispatch, commit }, { categoryId, payload }) {
+
+    //set promise
+    return new Promise((resolve, reject) => {
+
+        //store to Rest API "/api/admin/categories/:id" with method "POST"
+        this.$axios.post(`/api/admin/categories/${categoryId}`, payload)
+
+        //success
+        .then(() => {
+
+            //dispatch action "getCategoriesData"
+            dispatch('getCategoriesData')
+
+            //resolve promise
+            resolve()
+
+        })
+
+        //error
+        .catch(error => {
+            reject(error)
+        })
+
+    })
+},
 }
