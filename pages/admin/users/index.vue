306f409a39c8009482aff5a42e2b522b9029e12f
Farid Azhari <faridazhari07@gmail.com>
2024-06-22 04:20:32 +0700

    add halaman user

---
+++ pages/admin/users/index.vue
@@ -0,0 +1,96 @@
+<template>
+  <main class="c-main">
+    <div class="container-fluid">
+      <div class="fade-in">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="card border-0 rounded shadow-sm border-top-orange">
+              <div class="card-header">
+                <span class="font-weight-bold"><i class="fa fa-users"></i> USERS</span>
+              </div>
+              <div class="card-body">
+
+                <div class="form-group">
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <nuxt-link :to="{name: 'admin-users-create'}" class="btn btn-warning btn-sm" style="padding-top: 10px;">
+                            <i class="fa fa-plus-circle"></i> ADD NEW</nuxt-link>
+                        </div>
+                        <input type="text" class="form-control" placeholder="cari berdasarkan nama user">
+                        <div class="input-group-append">
+                            <button class="btn btn-warning"><i class="fa fa-search"></i>
+                            SEARCH
+                            </button>
+                        </div>
+                    </div>
+                </div>
+
+                <b-table striped bordered hover :items="users.data" :fields="fields" show-empty>
+                  <template v-slot:cell(image)="data">
+                      <img class="img-fluid" width="50" :src="data.item.image" />
+                  </template>
+                </b-table>
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+  export default {
+
+    //layout
+    layout: 'admin',
+
+    //meta
+    head() {
+      return {
+        title: 'Users - Administrator',
+      }
+    },
+
+    //data function
+    data() {
+      return {
+        //table header
+        fields: [{
+            label: 'User Name',
+            key: 'name'
+          },
+          {
+            label: 'Email Address',
+            key: 'email'
+          },
+          {
+            label: 'Actions',
+            key: 'actions',
+            tdClass: 'text-center'
+          }
+        ],
+      }
+    },
+
+    //hook "asyncData"
+    async asyncData({ store }) {
+        await store.dispatch('admin/user/getUsersData')
+    },
+
+    //computed
+    computed: {
+
+        //users
+        users() {
+            return this.$store.state.admin.user.users
+        },
+    },
+
+  }
+</script>
+
+<style>
+
+</style>
