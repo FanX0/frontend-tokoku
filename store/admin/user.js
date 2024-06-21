9705f230bffba24d2150dd9791cd74c93e6cc924
Farid Azhari <faridazhari07@gmail.com>
2024-06-22 05:04:05 +0700

    add edit user

--- store/admin/user.js
+++ store/admin/user.js
@@ -7,6 +7,9 @@ export const state = () => ({
   //page
   page: 1,
 
+  //user
+  user: {}
+
 })
 
 //mutations
@@ -24,6 +27,13 @@ export const mutations = {
 
       //set value state "page"
       state.page = payload
+  },
+
+  //mutation "SET_USER_DATA"
+  SET_USER_DATA(state, payload) {
+
+      //set value state "user"
+      state.user = payload
   },
 
 }
@@ -56,14 +66,67 @@ export const actions = {
       })
 
   },
-    //store user
-    storeUser({ dispatch, commit }, payload) {
+
+  //store user
+  storeUser({ dispatch, commit }, payload) {
 
       //set promise
       return new Promise((resolve, reject) => {
 
           //store to Rest API "/api/admin/users" with method "POST"
           this.$axios.post('/api/admin/users', payload)
+
+          //success
+          .then(() => {
+
+              //dispatch action "getUsersData"
+              dispatch('getUsersData')
+
+              //resolve promise
+              resolve()
+
+          })
+
+          //error
+          .catch(error => {
+              reject(error)
+          })
+
+      })
+  },
+
+  //get detail user
+  getDetailUser({ commit }, payload) {
+
+      //set promise
+      return new Promise((resolve, reject) => {
+
+          //get to Rest API "/api/admin/users/:id" with method "GET"
+          this.$axios.get(`/api/admin/users/${payload}`)
+
+          //success
+          .then(response => {
+
+              //commit to mutation "SET_USER_DATA"
+              commit('SET_USER_DATA', response.data.data)
+
+              //resolve promise
+              resolve()
+
+          })
+
+      })
+
+  },
+
+  //update user
+  updateUser({ dispatch, commit }, { userId, payload }) {
+
+      //set promise
+      return new Promise((resolve, reject) => {
+
+          //store to Rest API "/api/admin/users/:id" with method "POST"
+          this.$axios.post(`/api/admin/users/${userId}`, payload)
 
           //success
           .then(() => {
