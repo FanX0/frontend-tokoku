60444cf958bb2f325706538ef2074d0ff9c56bbb
Farid Azhari <faridazhari07@gmail.com>
2024-06-21 14:09:13 +0700

    add detail invoice

--- store/admin/invoice.js
+++ store/admin/invoice.js
@@ -7,6 +7,9 @@ export const state = () => ({
     //page
     page: 1,
 
+    //invoice
+    invoice: {}
+
 })
 
 //mutations
@@ -24,6 +27,13 @@ export const mutations = {
 
       //set value state "page"
       state.page = payload
+  },
+
+    //mutation "SET_INVOICE_DATA"
+    SET_INVOICE_DATA(state, payload) {
+
+      //set value state "invoice"
+      state.invoice = payload
   },
 
 }
@@ -51,6 +61,30 @@ export const actions = {
 
               //resolve promise
               resolve()
+          })
+
+      })
+
+  },
+
+     //get detail invoice
+     getDetailInvoice({ commit }, payload) {
+
+      //set promise
+      return new Promise((resolve, reject) => {
+
+          //get to Rest API "/api/admin/invoices/:id" with method "GET"
+          this.$axios.get(`/api/admin/invoices/${payload}`)
+
+          //success
+          .then(response => {
+
+              //commit to mutation "SET_INVOICE_DATA"
+              commit('SET_INVOICE_DATA', response.data.data)
+
+              //resolve promise
+              resolve()
+
           })
 
       })
