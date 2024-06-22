5e7a415d8756fa1cb9c0d0fafcbcbdfc31820e2a
Farid Azhari <faridazhari07@gmail.com>
2024-06-22 11:00:35 +0700

    add category header web

--- components/web/header.vue
+++ components/web/header.vue
@@ -5,8 +5,8 @@
         <div class="row align-items-center">
           <div class="col-lg-3 col-sm-4 col-md-4 col-5">
               <nuxt-link to="/" class="brand-wrap" data-abc="true">
-                <img src="/images/ias.png" width="35" class="bg-light p-2 rounded">
-                <span class="logo">Iastanti Glowscare</span>
+                <img src="/images/xiaomi.png" width="35" class="bg-light p-2 rounded">
+                <span class="logo">MI STORE</span>
               </nuxt-link>
           </div>
           <div class="col-lg-4 col-xl-5 col-sm-8 col-md-4 d-none d-md-block">
@@ -41,7 +41,18 @@
           aria-expanded="false"> <span class="navbar-toggler-icon"></span> </button>
         <div class="navbar-collapse collapse" id="dropdown6">
           <ul class="navbar-nav mr-auto">
-            <li class="nav-item"> <a href="#" class="nav-link" data-abc="true"><i class="fa fa-list-ul"></i> KATEGORI</a> </li>
+            <li class="nav-item dropdown"> <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown"
+                data-abc="true" aria-expanded="false"><i class="fa fa-list-ul"></i> KATEGORI</a>
+              <div class="dropdown-menu">
+                <nuxt-link :to="{name: 'categories-slug', params: {slug: category.slug}}" class="dropdown-item" v-for="category in categories" :key="category.id">
+                  <img :src="category.image" width="50"> {{ category.name }}
+                </nuxt-link>
+                <div class="dropdown-divider"></div>
+                <nuxt-link :to="{name: 'categories'}" class="dropdown-item active text-center" href="" data-abc="true">
+                  LIHAT SEMUA KATEGORI <i class="fa fa-long-arrow-alt-right"></i>
+                </nuxt-link>
+              </div>
+            </li>
             <li class="nav-item"> <a href="#" class="nav-link" data-abc="true"><i class="fa fa-shopping-bag"></i> SEMUA PRODUK</a> </li>
             <li class="nav-item"> <a href="#" class="nav-link" data-abc="true"><i class="fa fa-info-circle"></i> TENTANG</a> </li>
             <li class="nav-item"> <a href="#" class="nav-link" data-abc="true"><i class="fa fa-comments"></i> KONTAK</a> </li>
@@ -65,6 +76,21 @@
 <script>
   export default {
 
+    //hook "fetch"
+    async fetch() {
+
+      //fething sliders on Rest API
+      await this.$store.dispatch('web/category/getCategoriesData')
+    },
+
+    //computed
+    computed: {
+
+      //categories
+      categories() {
+        return this.$store.state.web.category.categories
+      },
+    }
 
   }
 </script>
