9aee5f4f893383fd32665dfde3a418001a069aed
Farid Azhari <faridazhari07@gmail.com>
2024-06-22 09:21:28 +0700

    add invoice customer

---
+++ pages/customer/invoices/index.vue
@@ -0,0 +1,115 @@
+<template>
+  <div class="container-fluid mt-custom">
+    <div class="fade-in">
+    <div class="row">
+      <div class="col-md-3">
+        <!-- sidebar -->
+        <Sidebar />
+        <!-- end sidebar -->
+      </div>
+      <div class="col-md-9">
+        <div class="card border-0 rounded shadow-sm border-top-orange">
+          <div class="card-body">
+            <h5><i class="fa fa-shopping-cart"></i> MY ORDERS</h5>
+            <hr>
+
+            <div class="form-group">
+              <div class="input-group mb-3">
+                <input type="text" class="form-control" placeholder="cari berdasarkan no. invoice">
+                <div class="input-group-append">
+                  <button class="btn btn-warning"><i class="fa fa-search"></i>
+                    SEARCH
+                  </button>
+                </div>
+              </div>
+            </div>
+
+            <b-table striped bordered hover :items="invoices.data" :fields="fields" show-empty>
+              <template v-slot:cell(grand_total)="row">
+                Rp. {{ formatPrice(row.item.grand_total) }}
+              </template>
+              <template v-slot:cell(status)="row">
+                <button v-if="row.item.status == 'pending'" class="btn btn-sm btn-primary"><i class="fa fa-circle-notch fa-spin"></i> {{ row.item.status }}</button>
+                <button v-if="row.item.status == 'success'" class="btn btn-sm btn-success"><i class="fa fa-check-circle"></i> {{ row.item.status }}</button>
+                <button v-if="row.item.status == 'expired'" class="btn btn-sm btn-warning-2"><i class="fa fa-exclamation-triangle"></i> {{ row.item.status }}</button>
+                <button v-if="row.item.status == 'failed'" class="btn btn-sm btn-danger"><i class="fa fa-times-circle"></i> {{ row.item.status }}</button>
+              </template>
+            </b-table>
+          </div>
+        </div>
+      </div>
+    </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  //import sidebar
+  import Sidebar from '@/components/web/sidebar.vue'
+
+  export default {
+
+    //middleware
+    middleware: 'isCustomer',
+
+    //layout
+    layout: 'default',
+
+    //register components
+    components: {
+      Sidebar
+    },
+
+    //meta
+    head() {
+      return {
+        title: 'Invoices - Customer',
+      }
+    },
+
+    //data function
+    data() {
+      return {
+        //table header
+        fields: [{
+            label: 'No. Invoice',
+            key: 'invoice'
+          },
+          {
+            label: 'Grand Total',
+            key: 'grand_total'
+          },
+          {
+            label: 'Status Payment',
+            key: 'status',
+            tdClass: 'text-center'
+          },
+          {
+            label: 'Actions',
+            key: 'actions',
+            tdClass: 'text-center'
+          }
+        ],
+      }
+    },
+
+    //hook "asyncData"
+    async asyncData({ store }) {
+      await store.dispatch('customer/invoice/getInvoicesData')
+    },
+
+    //computed
+    computed: {
+
+      //invoices
+      invoices() {
+        return this.$store.state.customer.invoice.invoices
+      },
+    },
+
+  }
+</script>
+
+<style>
+
+</style>
