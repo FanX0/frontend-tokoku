911f0d092835e7673c05521e18bbeb099816449c
Farid Azhari <faridazhari07@gmail.com>
2024-07-08 22:31:02 +0700

    konfigurasi fire base

--- pages/tentang/index.vue
+++ pages/tentang/index.vue
@@ -1,84 +1,77 @@
 <template>
-  <div class="container-fluid mt-custom">
-    <div class="fade-in">
-      <div class="row">
-<p>aaaa</p>
-
-
-      </div>
-
-        <!-- pagination -->
-        <div class="row justify-content-center mt-4 mb-4">
-          <div class="text-center">
-              <b-pagination align="center" :value="products.current_page" :total-rows="products.total"
-                  :per-page="products.per_page" @change="changePage" aria-controls="my-table"></b-pagination>
-          </div>
-      </div>
-
-    </div>
+  <div id="app">
+    <h1>Firebase Plugin Initialization</h1>
+    <p>Firebase has been initialized successfully.</p>
+    <p v-if="token">Device Token: {{ token }}</p>
   </div>
 </template>
 
 <script>
+import { initializeApp } from 'firebase/app';
+import { getMessaging, getToken, onMessage } from 'firebase/messaging';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAQuDGUwhr692CaUN9UZAzh833LdXj-qF8",
+  authDomain: "push-notification-57bbc.firebaseapp.com",
+  projectId: "push-notification-57bbc",
+  storageBucket: "push-notification-57bbc.appspot.com",
+  messagingSenderId: "1066753359582",
+  appId: "1:1066753359582:web:56de82e59dff3d0a778c4f",
+  measurementId: "G-8GWNSZ1LZP"
+};
+
+const app = initializeApp(firebaseConfig);
+
 export default {
+  name: 'App',
+  data() {
+    return {
+      token: null,
+      error: null
+    };
+  },
+  mounted() {
+    if (typeof window !== 'undefined') {
+      const messaging = getMessaging();
 
-    //meta
-    head() {
-      return {
-        title: 'IAASTANTI GLOWSCARE',
-        meta: [{
-            hid: 'og:title',
-            name: 'og:title',
-            content: 'Semua Produk - IAASTANTI GLOWSCARE'
-          },
-          {
-            hid: 'og:site_name',
-            name: 'og:site_name',
-            content: 'Semua Produk -IAASTANTI GLOWSCARE'
-          },
-          {
-            hid: 'og:image',
-            name: 'og:image',
-            content: '/images/logo.png'
-          },
-          {
-            hid: 'description',
-            name: 'description',
-            content: 'Semua Produk dari IAASTANTI GLOWSCARE'
-          },
-        ]
-      }
-    },
+      // Request permission and get token
+      Notification.requestPermission().then((permission) => {
+        if (permission === 'granted') {
+          getToken(messaging, { vapidKey: ' BBPJVv8-5e9M3CggcdRBzKk-efAkWzNOZB_S98a2DLtzheeFTz5NMob_jslEMs3PbovITc870YjqLeEkw5v1R58 ' })
+            .then((currentToken) => {
+              if (currentToken) {
+                console.log('Device token:', currentToken);
+                this.token = currentToken;
+                // Here you would send the token to your backend
+              } else {
+                console.error('No registration token available. Request permission to generate one.');
+              }
+            })
+            .catch((err) => {
+              console.error('An error occurred while retrieving token.', err);
+              this.error = err;
+            });
+        } else {
+          console.error('Unable to get permission to notify.');
+        }
+      });
 
-    //hook "asyncData"
-    async asyncData({ store }) {
-        await store.dispatch('web/product/getProductsData')
-    },
-
-    //computed
-    computed: {
-
-        //products
-        products() {
-            return this.$store.state.web.product.products
-        },
-    },
-     //method
-     methods: {
-
-    //method "changePage"
-    changePage(page) {
-
-      //commit to mutation "SET_PAGE"
-      this.$store.commit('web/product/SET_PAGE', page)
-
-      //dispatch on action "getProductsData"
-      this.$store.dispatch('web/product/getProductsData')
-    },
+      // Handle incoming messages
+      onMessage(messaging, (payload) => {
+        console.log('Message received. ', payload);
+        // Customize notification here
+      });
+    }
   }
-}
+};
 </script>
 
-<style>
-
+<style scoped>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  margin-top: 60px;
+}
 </style>
