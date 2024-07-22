961ce50ada22002566eb4eec165b0faafab9ff6c
Farid Azhari <faridazhari07@gmail.com>
2024-07-22 21:48:57 +0700

    ganti gambar dan tittle

--- pages/cart/index.vue
+++ pages/cart/index.vue
@@ -220,16 +220,16 @@
     //meta
     head() {
       return {
-        title: 'Cart - MI STORE - Distributor Xiaomi Indonesia Resmi',
+        title: 'Keranjang - Iaastanti Glowscare',
         meta: [{
             hid: 'og:title',
             name: 'og:title',
-            content: 'Cart - MI STORE - Distributor Xiaomi Indonesia Resmi'
+            content: 'Keranjang - Iaastanti Glowscare'
           },
           {
             hid: 'og:site_name',
             name: 'og:site_name',
-            content: 'Cart - MI STORE - Distributor Xiaomi Indonesia Resmi'
+            content: 'Keranjang - Iaastanti Glowscare'
           },
           {
             hid: 'og:image',
@@ -239,7 +239,7 @@
           {
             hid: 'description',
             name: 'description',
-            content: 'Cart - Official Toko Online Penjualan Produk Xiaomi'
+            content: 'Keranjang - Iaastanti Glowscare'
           },
         ]
       }
